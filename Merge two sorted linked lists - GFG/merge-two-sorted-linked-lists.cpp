d82a6d1b52e5662e9ba8652f334230453fe5aa2c
Ayush <ayushbhaimehta20002@gmail.com>
2023-01-28 16:13:15 +0530

    Added solution - LeetHub

--- Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.cpp
+++ Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.cpp
@@ -85,40 +85,37 @@ struct Node {
 Node* sortedMerge(Node* head1, Node* head2)  
 {  
     // code here
-    if(head1==NULL) 
-        return head2;
-    if(head2==NULL) 
-        return head1;
-    
-    Node *head=NULL,*curr=NULL;
-    if(head1->data>head2->data){
-        head=head2;
-        head2=head2->next;
-    }
-    else{
+    if(head1==NULL) return head2;
+    if(head2==NULL) return head1;
+    Node *curr=NULL,*head=NULL;
+    if(head1->data<head2->data){
         head=head1;
         head1=head1->next;
     }
-    
-    curr= head;
-        while(head1!=NULL && head2!=NULL){
-            if(head1->data>head2->data){
-                curr->next=head2;
-                curr=curr->next;
-                head2=head2->next;
-            }
-            else{
-                curr->next=head1;
-                curr= curr->next;
-                head1=head1->next;
-
-            }
+    else{
+        head=head2;
+        head2=head2->next;
+    }
+    curr=head;
+    while(head1!=NULL && head2!=NULL){
+        if(head1->data<head2->data){
+            curr->next=head1;
+            head1=head1->next;
+            curr=curr->next;
         }
-        if(head1==NULL){
+        else{
             curr->next=head2;
+            head2=head2->next;
+            curr=curr->next;
         }
-        else
+    }
+    
+    if(head1==NULL){
+        curr->next=head2;
+    }
+    if( head2==NULL){
         curr->next=head1;
-        
+    }
+    
     return head;
 }  
