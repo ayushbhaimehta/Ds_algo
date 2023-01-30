91b231b249fa1d15209ea75642fcc7ec3f65e640
Ayush <ayushbhaimehta20002@gmail.com>
2023-01-31 00:10:22 +0530

    Time: 46 ms (55.95%), Space: 20.1 MB (29.54%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
-        int n=arr.size();
-        sort(arr.begin(),arr.end());
-        int left=arr[0][0];
-        int right=arr[0][1];
-        vector<vector<int>>ans;
         
+        // [1 9] [11 18] [19 20]
+        
+        int n=arr.size();  // 5
+        sort(arr.begin(),arr.end());
+        vector<vector<int>>ans;
+        // left 1 right 3
+        int left=arr[0][0]; // 1
+        int right=arr[0][1]; // 2
+        
+        // trtaver from element i-1
         for(int i=1;i<n;i++){
-            if(arr[i][0]<=right){
+            if(right>=arr[i][0]){
                 right=max(right,arr[i][1]);
             }
             else{
-                vector<int>temp(2);
-                temp[0]=left;
-                temp[1]=right;
+                vector<int>temp;
+                temp.push_back(left);
+                temp.push_back(right);
                 ans.push_back(temp);
                 left=arr[i][0];
                 right=arr[i][1];
             }
         }
-        vector<int>temp(2);
-        temp[0]=left;
-        temp[1]=right;
+        vector<int>temp;
+        temp.push_back(left);
+        temp.push_back(right);
         ans.push_back(temp);
         return ans;
     }
