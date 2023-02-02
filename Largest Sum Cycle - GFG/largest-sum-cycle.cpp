268aa915dac930a11d933acd66f74d1f4ce003c2
Ayush <ayushbhaimehta20002@gmail.com>
2023-02-02 13:19:24 +0530

    Added solution - LeetHub

--- Largest Sum Cycle - GFG/largest-sum-cycle.cpp
+++ Largest Sum Cycle - GFG/largest-sum-cycle.cpp
@@ -5,48 +5,30 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
+#define ll long long
 class Solution
 {
   public:
-  long long largestSumCycle(int n, vector<int> edge)
+  long long largestSumCycle(int N, vector<int> e)
   {
-    vector<int> indegree(n, 0);
-        for(auto it: edge) {
-            if(it == -1) continue;
-            indegree[it]++;
+    // code here
+     ll ans = -1;
+    vector<int> vis(N, 0);
+    function<ll(int)> dfs = [&](int i){
+        vis[i] = 1;
+        ll s = 0;
+        if(e[i] != -1){
+            s = i;
+            if(vis[e[i]]) vis[e[i]] = 2;
+            else s += dfs(e[i])  ;
         }
-        
-        queue<int> q;
-        vector<int> vis(n, false);
-        for(int i = 0; i < n; i++) {
-            if(indegree[i] == 0) {
-                vis[i] = true;
-                q.push(i);
-            }
-        }
-        
-        while(q.empty() == false) {
-            int curr = q.front(); q.pop();
-            int par = edge[curr];
-            if(par == -1) continue;
-            
-            if(--indegree[par] == 0) {
-                q.push(par);
-                vis[par] = true;
-            }
-        }
-        
-        int res = -1;
-        for(int i = 0; i < n; i++) {
-            if(vis[i]) continue;
-            int val = 0;
-            for(int st = i; vis[st] == false; st = edge[st]) {
-                val += st;
-                vis[st] = true;
-            }
-            res = max(res, val);
-        }
-        return res;
+        if(vis[i] == 2) ans = max(ans, s), s = 0;
+        return s;
+    };
+    
+    for(int i = 0; i < N; i++)
+        if(!vis[i]) dfs(i);
+    return ans;
   }
 };
 
