989e1c6846138712a6a99009272847a09643f11c
Ayush <ayushbhaimehta20002@gmail.com>
2023-03-11 20:44:22 +0530

    Updated Solution - GfG to GitHub

--- Medium/Kth smallest element/kth-smallest-element.cpp
+++ Medium/Kth smallest element/kth-smallest-element.cpp
@@ -13,55 +13,22 @@ class Solution{
     // l : starting index of the array i.e 0
     // r : ending index of the array i.e size-1
     // k : find kth smallest element and return using this function
-     void swap(int *a, int *b)
-    {
-        int temp = *a;
-        *a = *b;
-        *b = temp;
-    }
-    // finding the pivot element and partition the array along that
-    int randomPartition(int arr[], int l, int r)
-    {
-        int n = r-l+1;
-        int pivot = rand() % n;
-        swap(&arr[l + pivot], &arr[r]);
-        return partition(arr, l, r);
-    }
-    int kthSmallest(int arr[], int l, int r, int k)
-    {
-        // If k is smaller than number of elements in array
-        if (k > 0 && k <= r - l + 1)
-        {
-            // find a position for random partition
-            int pos = randomPartition(arr, l, r);
-            
-            // if this pos gives the kth smallest element, then return
-            if (pos-l == k-1)
-                return arr[pos];
-                
-            // else recurse for the left and right half accordingly
-            if (pos-l > k-1)  
-                return kthSmallest(arr, l, pos-1, k);
-            return kthSmallest(arr, pos+1, r, k-pos+l-1);
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        int n=r+1;
+        priority_queue<int>pq;
+        for(int i=0;i<k;i++){
+            pq.push(arr[i]);
         }
-    
-        return INT_MAX;
-    }
-     
-    // partitioning the array along the pivot
-    int partition(int arr[], int l, int r)
-    {
-        int x = arr[r], i = l;
-        for (int j = l; j <= r - 1; j++)
-        {
-            if (arr[j] <= x)
-            {
-                swap(&arr[i], &arr[j]);
-                i++;
+        
+        for(int i=k;i<n;i++){
+            if(arr[i]<pq.top()){
+                pq.pop();
+                pq.push(arr[i]);
             }
         }
-        swap(&arr[i], &arr[r]);
-        return i;
+        
+        return pq.top();
     }
 };
 
