9cefd0e387fd0b6b35f0c6e691deabc35a74bbd7
Ayush <ayushbhaimehta20002@gmail.com>
2023-03-25 19:14:53 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Intersection of two arrays/intersection-of-two-arrays.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking count of total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n, m;
+
+            // taking count of elements in array a
+            n = sc.nextInt();
+
+            // taking count of elements in array b
+            m = sc.nextInt();
+
+            // Creating 2 arrays of n and m
+            int a[] = new int[n];
+            int b[] = new int[m];
+
+            // inserting elements to array a
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            // inserting elements to array b
+            for (int i = 0; i < m; i++) {
+                b[i] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            // calling NumberofElementsInIntersection method
+            // and printing the result
+            System.out.println(ob.NumberofElementsInIntersection(a, b, n, m));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    public static int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code here
+        Map<Integer,Integer> mp = new HashMap<>();
+        int count =0;
+        int min = Math.min(m,n);
+        for(int i=0;i<min;i++){
+            if(min == n){
+                mp.put(a[i],1);
+            }
+            else{
+                mp.put(b[i],1);
+            }
+        }
+        for(int i : min==n?b:a){
+            if(mp.get(i)!=null){
+                if(mp.get(i)==1){
+                   count++;
+                   mp.put(i,0); 
+                }
+            }
+        }
+        return count;
+    }
+};
